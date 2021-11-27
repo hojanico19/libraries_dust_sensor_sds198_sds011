419d00020d74a999d7253e92d2df990877e92fab
hojanico19 <90467313+hojanico19@users.noreply.github.com>
2021-11-27 00:48:42 -0300

    Update SDS011.h

--- SDS011-master/SDS011.h
+++ SDS011-master/SDS011.h
@@ -1,13 +1,3 @@
-// SDS011 dust sensor PM2.5 and PM10
-// ---------------------------------
-//
-// By R. Zschiegner (rz@madavi.de)
-// April 2016
-//
-// Documentation:
-//		- The iNovaFitness SDS011 datasheet
-//
-
 #if ARDUINO >= 100
 	#include "Arduino.h"
 #else
